e84b6ea375737fa9af8f8574342cfd6f6357fd1c
Sara-Luongo <saraluongo202002@gmail.com>
2026-05-13 18:09:41 +0200

    creata card con un pò di stile bootstrap

--- src/pagine/Prodotto.jsx
+++ src/pagine/Prodotto.jsx
@@ -17,8 +17,34 @@ function Prodotto() {
             })
     }, [prodottiId])
 
-    return <div>
+    return <div className="card h-100 shadow-lg mt-4">
 
+        <img
+            src={prodottoSingolo.image}
+            className="card-img-top p-3"
+            alt={prodottoSingolo.title}
+            style={{ height: "250px", objectFit: "contain" }}
+        />
+
+        <div className="card-body d-flex flex-column">
+
+            <h5 className="card-title text-primary">
+                {prodottoSingolo.title}
+            </h5>
+
+
+            <p className="card-text text-muted">
+                {prodottoSingolo.description}
+            </p>
+
+
+            <div className="mt-auto">
+                <span className="fs-4 fw-bold text-primary">
+                    {prodottoSingolo.price} &euro;
+                </span>
+            </div>
+
+        </div>
     </div>
-};
+}
 export default Prodotto
